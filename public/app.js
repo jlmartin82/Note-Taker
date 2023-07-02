c9e800af05417a882e460d44c568b34754953025
Justin <justin.martin@hsc.utah.edu>
2023-07-02 07:52:34 -0600

    app post function

--- public/app.js
+++ public/app.js
@@ -3,7 +3,16 @@ const fs = require('fs');
 
 const app = express();
 
-app.get('/notes', (req,res =>{
+app.get('/notes', (req,res) =>{
     const notes = fs.readFileSync('./db.json', 'utf8');
     res.setEncoding(note);
-}))
+});
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body;
+    const db = require('./db.json'); 
+    const newNotes = [...db.notes, note]; 
+    fs.writeFileSync('./db.json', JSON.stringify({ notes: newNotes }, null, 2)); 
+    res.send(note);
+  });
+  
